dafc9b86633809539477dee28b00b8d085aff2de
Tom Smith <wisdev53548@hotmail.com>
2018-01-23 19:10:59 -0600

    Added /profile/:id and /image routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const database = {
 			joined: new Date()
 		},
 		{
-			id: '122',
+			id: '124',
 			name: 'Sally',
 			email: 'sally@gmail.com',
 			password: 'bananas',
@@ -49,6 +49,35 @@ app.post('/register', (req, res) => {
 		joined: new Date()
 	})
 	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params
+	let found = false
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true
+			return res.json(user)
+		}
+	})
+	if (!found) {
+		res.status(400).json('not found')
+	}
+})
+
+app.post('/image', (req, res) => {
+	const { id } = req.body
+	let found = false
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true
+			user.entries++
+			return res.json(user.entries)
+		}
+	})
+	if (!found) {
+		res.status(400).json('not found')
+	}
 })
 
 app.listen(3000, () => {
