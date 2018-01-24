b898c1468add5ffa6dfd3906bbfe7dbdcef09187
Tom Smith <wisdev53548@hotmail.com>
2018-01-23 18:18:17 -0600

    Updated endpoints and routes planned in server.js document

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
 	console.log('Server is running on port 3000')
 })
+
+
+
+
+/*
+ENDPOINT AND ROUTE PLANS
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = updated user or account
+*/
