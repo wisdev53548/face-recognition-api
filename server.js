56630a8c6ed25ade58bb93b6bef7503dc1b7b5ea
Tom Smith <wisdev53548@hotmail.com>
2018-01-23 18:22:26 -0600

    Signin route added with test string

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('This is working')
 })
 
+app.post('/signin', (req, res) => {
+	res.json('signin')
+})
+
 app.listen(3000, () => {
 	console.log('Server is running on port 3000')
 })
