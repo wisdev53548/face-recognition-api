9027e278689002eaa1765a503dd270f860ff5b4f
Tom Smith <wisdev53548@hotmail.com>
2018-01-23 18:13:16 -0600

    Testing server and route in POSTman

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+	res.send('This is working')
+})
+
 app.listen(3000, () => {
-	console.log('app is running')
+	console.log('Server is running on port 3000')
 })
