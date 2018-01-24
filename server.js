293f603d405e2b65f6bb81c257277f45b3e4a205
Tom Smith <wisdev53548@hotmail.com>
2018-01-23 18:49:01 -0600

    Register route added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-	res.send('This is working')
+	res.send(database.users)
 })
 
 app.post('/signin', (req, res) => {
@@ -36,6 +36,19 @@ app.post('/signin', (req, res) => {
 	} else {
 		res.status(400).json('error loggin in')
 	}
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1])
 })
 
 app.listen(3000, () => {
